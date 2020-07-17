c5976eb5226659c3a5243bd24b636937f8d5d4fb
Renato Ferreira <renatoferreira.dev@gmail.com>
2020-07-17 21:26:55 +0200

    Implement CreateTransactionService service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,17 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw Error('Transaction rejected! Insufficient funds.');
+    }
+
+    this.transactionsRepository.create({ title, value, type });
+
+    return transaction;
   }
 }
 
